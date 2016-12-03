388c85e4503ac197f36e5bede72d70ae0a6ab96e
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-12-03 18:54:52 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,14 @@
 set -e
 set -xv
 
-systemctl start elassandra
-sleep 5
-
 # Patch demo kibi to use standard ES port
 perl -p -i -e "s/9220/9200/" /opt/kibi/config/kibi.yml
 perl -p -i -e "s/localhost/0.0.0.0/" /opt/kibi/config/kibi.yml
 
 # Start Kibi
-/etc/init.d/kibi start
-tail -f /var/log/elasticsearch/*.log
+/opt/kibi/bin/kibi >> /opt/elassandra/logs/kibi.log &
+
+# Start Elassandra
+/opt/elassandra/bin/elassandra start
+
+tail -f /opt/elassandra/logs/*
