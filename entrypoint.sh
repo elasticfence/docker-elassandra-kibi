3c3b73af8cfd716ac19b3b3dd4e1b0323c27050a
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-12-22 22:29:16 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,8 @@ swapoff -a
 echo "Starting Elassandra... "
 export CASSANDRA_HOME=/opt/elassandra
 source $CASSANDRA_HOME/bin/aliases.sh
-$CASSANDRA_HOME/bin/cassandra -e > /var/log/elassandra.log &
-sleep 10
-$CASSANDRA_HOME/bin/nodetool status
+$CASSANDRA_HOME/bin/cassandra -e &
+sleep 5
 
 # Patch demo kibi to use standard ES port
 perl -p -i -e "s/9220/9200/" /opt/kibi/config/kibi.yml
@@ -18,6 +17,11 @@ perl -p -i -e "s/localhost/172.17.0.2/" /opt/kibi/config/kibi.yml
 
 # Start Kibi
 echo "Starting Kibi... "
-/opt/kibi/bin/kibi >> /opt/elassandra/logs/kibi.log &
+/opt/kibi/bin/kibi >> $CASSANDRA_LOGS/kibi.log &
 
-tail -f /var/log/elassandra.log
+# Get Cassandra Status
+$CASSANDRA_HOME/bin/nodetool status &
+# Get ES Status
+status
+
+tail -f $CASSANDRA_LOGS/system.log
