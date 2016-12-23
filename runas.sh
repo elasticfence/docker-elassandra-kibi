0f600560f25c66c48b413b8523c53117da5edace
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-12-24 00:00:51 +0100

    Update runas.sh

--- runas.sh
+++ runas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Initiate reverse proxy
-reverse-proxy --port 9222 --target 127.0.0.1:9200
+reverse-proxy --port 9222 --target 127.0.0.1:9200 &
 # Execute Entrypoint as Kibi user
 /bin/su - kibi -c "/opt/entrypoint.sh"
